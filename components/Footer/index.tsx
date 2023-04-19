ffcc2683bf4dc65ecff53030c33df66cf9e0234f
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-04-19 17:48:30 +0200

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -14,15 +14,18 @@ export const Footer = () => {
         <p className={styles.logoText}>
           <img src={"images/logoDiscord.svg"} className={styles.logo} alt="logoDiscord" />
           <span><a href="https://discord.gg/3KFcCyNZjU" className={styles.link}>Discord</a>, </span>
-          <img src={"images/logoGithubd.svg"} className={styles.logo} alt="logoGithub" />
-          <span><a href="https://github.com/Lunastellia/AionClassicFR" className={styles.link}>Github</a> Par Lunastellia.      ---      </span>
+          <img src={"images/logoGithub.svg"} className={styles.logo} alt="logoGithub" />
+          <span><a href="https://githubd.com/Lunastellia/AionClassicFR" className={styles.link}>Github</a> Par Lunastellia.     ---     </span>
+        </p>
+      </section>
+      <section className={styles.container}>
+        <p className={styles.logoText}>
           <span>Ai</span>
           <img src={"images/aion.svg"} className={styles.logo} alt="o" />
           <span>n </span>
           <span><a href="https://persepha.github.io/Aion-classic-stigma-calculator/" className={styles.link}>Version Original</a> (Anglais) by <a href="https://github.com/Persepha/Aion-classic-stigma-calculator" className={styles.link}>Persepha</a>.</span>
-          <br />
         </p>
+      </section>
     </footer>
   );
 };
-
