cb22c3984f2eca045a32ec2705f4009c39719929
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-04-19 17:01:23 +0200

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,27 +5,21 @@ import cn from "classnames";
 import styles from "./Footer.module.css";
 
 const font = Roboto({
-  weight: "500",
-  subsets: ["latin"],
+    weight: "500",
+    subsets: ["latin"],
 });
 
 export const Footer = () => {
-  return (
-    <footer className={cn(font.className, styles.footer)}>
-      <section className={styles.container}>
-        <p className={styles.logoText}>
-          <span>AION Stigmas Builder (Classic 2.4) FR Modifier par Lunastellia</span>
-        </p>
-        <span className={styles.secondaryText}> 
-          <p className={styles.logoText}>
-          <span>Ai</span>
-          <img src={"images/aion.svg"} className={styles.logo} alt="o" />
-          <span>n </span>
-          <span> classic 2.4</span>
-           <span> <a href="https://persepha.github.io/Aion-classic-stigma-calculator/" > Version original en Anglais, par persepha</a>stigma calculator</span>
-        </p>
-          
-      </section>
-    </footer>
-  );
+    return (
+        <footer className={cn(font.className, styles.footer)}>
+            <section className={styles.container}>
+                <p className={styles.logoText}>
+                    <span>Ai</span>
+                    <img src={"images/aion.svg"} className={styles.logo} alt="o" />
+                    <span>n </span>
+                    <span><a href="https://persepha.github.io/Aion-classic-stigma-calculator/" className={styles.link}>Version Original</a> (Anglais) by <a href="https://github.com/Persepha/Aion-classic-stigma-calculator" className={styles.link}>Persepha</a>.</span>
+                </p>
+            </section>
+        </footer>
+    );
 };
