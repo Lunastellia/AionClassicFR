bb2195f14f198570f7edd744042092a9276438d2
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-04-19 17:17:19 +0200

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -14,10 +14,13 @@ export const Footer = () => {
         <footer className={cn(font.className, styles.footer)}>
             <section className={styles.container}>
                 <p className={styles.logoText}>
-                    <span>Ai</span>
-                    <img src={"images/aion.svg"} className={styles.logo} alt="o" />
-                    <span>n </span>
-                    <span><a href="https://persepha.github.io/Aion-classic-stigma-calculator/" className={styles.link}>Version Original</a> (Anglais) by <a href="https://github.com/Persepha/Aion-classic-stigma-calculator" className={styles.link}>Persepha</a>.</span>
+                    <img src={"images/logoDiscord.png"} className={styles.logo} alt="logoDiscord" />
+                    <span><a href="https://discord.gg/3KFcCyNZjU" className={styles.link}> Discord</a>
+                       <span>, </span> 
+                     <img src={"images/logoGithub.png"} className={styles.logo} alt="logoGithub" />
+                    <span><a href="https://github.com/Lunastellia/AionClassicFR" className={styles.link}> Github</a>
+                    <span> Par Lunastellia.</span> 
+                    <br />
                 </p>
             </section>
             <section className={styles.container}>
