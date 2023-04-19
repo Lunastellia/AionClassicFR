3674533d21dc8eceab7ea415f033213f136c5da8
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-04-19 17:39:02 +0200

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -14,8 +14,9 @@ export const Footer = () => {
         <p className={styles.logoText}>
           <img src={"images/logoDiscord.svg"} className={styles.logo} alt="logoDiscord" />
           <span><a href="https://discord.gg/3KFcCyNZjU" className={styles.link}>Discord</a>, </span>
-          <img src={"images/logoGithub.svg"} className={styles.logo} alt="logoGithub" />
-          <span><a href="https://github.com/Lunastellia/AionClassicFR" className={styles.link}>Github</a> Par Lunastellia.</span>
+          <img src={"images/logoGithubd.svg"} className={styles.logo} alt="logoGithub" />
+          <span><a href="https://github.com/Lunastellia/AionClassicFR" className={styles.link}>Github</a> Par Lunastellia. --- </span>
+          <span className={styles.secondaryText}>c</span>
           <br />
         </p>
       </section>
