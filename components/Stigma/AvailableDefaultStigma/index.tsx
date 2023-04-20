630003f80567ca77b38ea786fe6ddcdad054b9da
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-04-20 10:11:38 +0200

    Update index.tsx

--- components/Stigma/AvailableDefaultStigma/index.tsx
+++ components/Stigma/AvailableDefaultStigma/index.tsx
@@ -88,7 +88,7 @@ export const AvailableDefaultStigma: NextPage<AvailableDefaultStigmaProps> = ({
 
           <Image
             onClick={handleClick}
-            src={`images/${selectedClass}/${stigma.stigma[stigmaLvl].icon}.png`}
+            src={`images/${selectedClass}/${stigma.stigma[stigmaLvl].name}.png`}
             alt={`${stigma.stigma[stigmaLvl].name} icon`}
             height={40}
             width={40}
